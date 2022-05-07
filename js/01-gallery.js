3233106af40be2565787dc9e0e96a441f270e03f
Anna Melnichuk <varchuk.anna.94@gmail.com>
2022-05-07 11:59:16 +0300

    HW8 is done

---
+++ js/01-gallery.js
@@ -0,0 +1,25 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Change code below this line
+
+const galleryEl = document.querySelector('.gallery');
+galleryEl.style.listStyleType = 'none';
+
+function createGalleryItem(cards) {
+    return cards.map(({ original, preview, description }) => {
+        return `<li><a class="gallery__item" href="${original}">
+   <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li> `
+    }).join('');
+};
+
+galleryEl.insertAdjacentHTML('afterbegin', createGalleryItem(galleryItems));
+
+  const lightbox = new SimpleLightbox(`.gallery a`, {
+      captions: true,
+      captionsData: `alt`,
+      captionsDelay: 250,
+  });
+
